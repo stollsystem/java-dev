76df6a8fd78f6950c50d408a4f3e30652fe3546c
Carlos Gomes <supersuporte@gmail.com>
2014-09-12 16:25:45 -0300

    Tela de inclusão de produto.

--- java-dev/src/main/java/br/com/carlosgomes/controller/ProdutoManagedBean.java
+++ java-dev/src/main/java/br/com/carlosgomes/controller/ProdutoManagedBean.java
@@ -2,8 +2,10 @@ package br.com.carlosgomes.controller;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.carlosgomes.dao.ProdutoDAO;
 import br.com.carlosgomes.modelo.Produto;
@@ -12,11 +14,28 @@ import br.com.carlosgomes.modelo.Produto;
 @ViewScoped
 public class ProdutoManagedBean {
 	
+	private Produto produto = new Produto();
 	private ProdutoDAO produtoDAO = new ProdutoDAO();
 	
 	public List<Produto> getProdutos() {
 		List<Produto> produtos = produtoDAO.busca();
 		return produtos;
+	}
+	
+	public void salva() {
+		produtoDAO.beginTransaction();
+		produtoDAO.salva(produto);
+		produtoDAO.commit();
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Produto cadastrado com sucesso!"));
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
 	}
 
 }
