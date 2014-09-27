0d308e669a97978e9f6da11a8dc6fdbfa5d4e170
Carlos Gomes <supersuporte@gmail.com>
2014-09-27 11:32:44 -0300

    Alteracao de Produto

--- java-dev/src/main/java/br/com/carlosgomes/controller/ProdutoManagedBean.java
+++ java-dev/src/main/java/br/com/carlosgomes/controller/ProdutoManagedBean.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 
 import br.com.carlosgomes.dao.ProdutoDAO;
 import br.com.carlosgomes.modelo.Produto;
