fcf702ee263f90ae1cc81e415feab967ccfaec0a
Carlos Gomes <supersuporte@gmail.com>
2014-09-27 15:51:13 -0300

    Refactor do teste unitário e do persistence.xml

--- java-dev/src/test/java/br/com/carlosgomes/produto/ProdutoDAOTest.java
+++ java-dev/src/test/java/br/com/carlosgomes/produto/ProdutoDAOTest.java
@@ -2,28 +2,29 @@ package br.com.carlosgomes.produto;
 
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
 import br.com.carlosgomes.dao.ProdutoDAO;
 import br.com.carlosgomes.modelo.Produto;
+import br.com.carlosgomes.util.HibernateUtil;
 
-@Ignore
 public class ProdutoDAOTest {
 	
-	private ProdutoDAO produtoDAO = new ProdutoDAO();
+	private ProdutoDAO produtoDAO;
+	
+	@Before
+	public void init() {
+		HibernateUtil hibernateUtil = new HibernateUtil();
+		hibernateUtil.setPersistenceUnitName("java-dev-test");
+		produtoDAO = new ProdutoDAO();
+	}
 	
 	@Test
-	public void inclui() {
-		Produto produto1 = new Produto();
-		produto1.setDescricao("Produto Teste 001");
-		produtoDAO.salva(produto1);
-
-		Produto produto2 = new Produto();
-		produto2.setDescricao("Produto Teste 002");
-		produtoDAO.salva(produto2);
-
-//		assertEquals("Produto 1 não consistido corretamente", produto1.getDescricao(), produtoDAO.buscaProduto(produto1.getDescricao()));
-//		assertEquals("Produto 2 não consistido corretamente", produto2.getDescricao(), produtoDAO.buscaProduto(produto2.getDescricao()));
+	public void busca() {
+		List<Produto> produtos = produtoDAO.busca();
+		assertNotNull("Lista de produtos retornou nulo", produtos);
 	}
-
 }
